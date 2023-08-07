c19d3067a0fe43f2382668d048776405fc23700d
Jesus Puentes <jopt98@gmail.com>
2023-08-07 15:51:50 -0600

    Se ordenó el contexto y se creó la ruta privada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
-import useComponentVisible from './hooks/useComponentVisible';
-import useFetch from './hooks/useFetch';
-import useFilter from './hooks/useFilter';
-import useMenu from './hooks/useMenu';
-import useOrder from './hooks/useOrder';
-import HomeScreen from './hoc/HomeScreen';
 import { AppContext } from './hoc/Context';
-import useStatus from './hooks/useStatus';
+import { HomeScreenRouter } from './routers/HomeScreenRouter';
+import { PrivateRoute } from './routers/PrivateRouter';
 import {
   BrowserRouter as Router,
   Routes,
@@ -17,45 +12,17 @@ import LoginScreen from './hoc/LoginScreen';
 
 function App() {
 
-  const [ ToggleMenu, handleToggleMenu ] = useMenu();
-
-  const [ ref, isComponentVisible, setIsComponentVisible ] = useComponentVisible(false);
-
-  const [ refOrder, isComponentVisibleOrder, setIsComponentVisibleOrder ] = useComponentVisible(false);
-
-  const [ Data, setData ] = useFetch(`${process.env.REACT_APP_API_URL}/api/feedback`);
-
-  const [ Order, handleOrderChange ] = useOrder( setIsComponentVisibleOrder, Data, setData );
-
-  const [ Filter, handleChangeFilter, ContentExists ] = useFilter(Data, setIsComponentVisible);
-
-  const Statuses = useStatus( Data );
-
-  const context = {
-    ToggleMenu,
-    handleToggleMenu,
-    ref,
-    isComponentVisible,
-    setIsComponentVisible,
-    Data,
-    Order,
-    handleOrderChange,
-    refOrder,
-    isComponentVisibleOrder, 
-    setIsComponentVisibleOrder,
-    Filter,
-    handleChangeFilter,
-    Statuses,
-    ContentExists
-  }
-
   return (
-    <AppContext.Provider value={ context }>
+    <AppContext.Provider>
       <div className="App">
         <Router>
           <Routes>
-            <Route  path='login' element={ <LoginScreen /> }/>
-            <Route path='/' element={ <HomeScreen /> } />
+            <Route path='login/*' element={ <LoginScreen /> }/>
+            <Route path='/*' element={
+              <PrivateRoute child={
+                <HomeScreenRouter />}
+              />
+            }/>
           </Routes>
         </Router>
       </div>
--- src/routers/HomeScreenRouter.js
+++ src/routers/HomeScreenRouter.js
@@ -1,13 +1,56 @@
 import { Route, Routes } from 'react-router-dom';
+import HomeScreen from '../hoc/HomeScreen';
+import ElementScreen from '../hoc/ElementScreen';
+import useComponentVisible from '../hooks/useComponentVisible';
+import useFetch from '../hooks/useFetch';
+import useFilter from '../hooks/useFilter';
+import useMenu from '../hooks/useMenu';
+import useOrder from '../hooks/useOrder';
+import useStatus from '../hooks/useStatus';
+import { AppContext } from '../hoc/Context';
 
 export const HomeScreenRouter = () => {
 
+  const [ ToggleMenu, handleToggleMenu ] = useMenu();
+
+  const [ ref, isComponentVisible, setIsComponentVisible ] = useComponentVisible(false);
+
+  const [ refOrder, isComponentVisibleOrder, setIsComponentVisibleOrder ] = useComponentVisible(false);
+
+  const [ Data, setData ] = useFetch(`${process.env.REACT_APP_API_URL}/api/feedback`);
+
+  const [ Order, handleOrderChange ] = useOrder( setIsComponentVisibleOrder, Data, setData );
+
+  const [ Filter, handleChangeFilter, ContentExists ] = useFilter(Data, setIsComponentVisible);
+
+  const Statuses = useStatus( Data );
+
+  const context = {
+    ToggleMenu,
+    handleToggleMenu,
+    ref,
+    isComponentVisible,
+    setIsComponentVisible,
+    Data,
+    Order,
+    handleOrderChange,
+    refOrder,
+    isComponentVisibleOrder, 
+    setIsComponentVisibleOrder,
+    Filter,
+    handleChangeFilter,
+    Statuses,
+    ContentExists
+  }
 
     return (
         <>
-            <Routes>
-                
-            </Routes>
+            <AppContext.Provider value={ context }>
+                <Routes>
+                    <Route path='/element' element={ <ElementScreen /> } />
+                    <Route path='/*' element={ <HomeScreen /> } />
+                </Routes>
+            </AppContext.Provider>
         </>
     )
 }
